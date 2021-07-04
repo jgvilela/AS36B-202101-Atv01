1a061cb69092eeac639edb0b7ca4e9bd01c6a2bf
João Gabriel Vilela <Sknej@users.noreply.github.com>
2021-07-04 16:48:47 -0300

    Criação do código
    Criação dos códigos que permitem um servidor a conexão de diversos clientes para comunicação entre si.

---
+++ Client.py
@@ -0,0 +1,78 @@
+#! /usr/bin/env python
+
+import socket
+import sys
+import time
+import threading
+import select
+import traceback
+
+
+class Server(threading.Thread):
+    def initialise(self, receive):
+        self.receive = receive
+
+    def run(self):
+        lis = []
+        lis.append(self.receive)
+        while 1:
+            read, write, err = select.select(lis, [], [])
+            for item in read:
+                try:
+                    s = item.recv(1024)
+                    if s != '':
+                        chunk = s
+                        print(chunk.decode() + '\n>>')
+                except:
+                    traceback.print_exc(file=sys.stdout)
+                    break
+
+
+class Client(threading.Thread):
+    def connect(self, host, port):
+        self.sock.connect((host, port))
+
+    def client(self, host, port, msg):
+        sent = self.sock.send(msg)
+        # print "Sent\n"
+
+    def run(self):
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.setsockopt(socket.IPPROTO_TCP, socket.TCP_NODELAY, 1)
+        try:
+            host = input("Digite o IP do servidor:\n>>")
+            port = int(input("Digite a porta de conexão ao servidor:\n>>"))
+        except EOFError:
+            print("Erro")
+            return 1
+
+        print("Conectando\n")
+        s = ''
+        self.connect(host, port)
+        print("Conectado\n")
+        user_name = input("Digite o nome do usuário a ser utilizado:\n>>")
+        receive = self.sock
+        time.sleep(1)
+        srv = Server()
+        srv.initialise(receive)
+        srv.daemon = True
+        print("Iniciando Serviço")
+        srv.start()
+        while 1:
+            # print "Waiting for message\n"
+            msg = input('>>')
+            if msg == 'saída':
+                break
+            if msg == '':
+                continue
+            # print "Sending\n"
+            msg = user_name + ': ' + msg
+            data = msg.encode()
+            self.client(host, port, data)
+        return (1)
+
+
+if __name__ == '__main__':
+    print("Iniciando Cliente")
+    cli = Client()
+    cli.start()
